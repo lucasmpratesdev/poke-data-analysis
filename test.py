466fc43877c699a34289e9d2807bf876b92e07f8
Lucas Prates <lucas.prates@matrixdobrasil.com.br>
2025-01-15 17:47:53 -0300

    adding task 2 Data Transformation and task 3 Report and Export

--- test.py
+++ test.py
@@ -37,6 +37,7 @@ for pokemon in pokemon_list:
 
 # Criar DataFrame com os dados coletados
 pokemon_df = pd.DataFrame(pokemon_details)
+print(pokemon_df)
 
 # Categorizar os Pokémon por experiência base
 def categorize_experience(exp):
@@ -51,25 +52,36 @@ pokemon_df["Category"] = pokemon_df["Base Experience"].apply(categorize_experien
 
 # Explodir os tipos em múltiplas linhas
 types_df = pokemon_df.explode("Types")
-
+print(types_df)
 # Contagem de Pokémon por tipo
 type_counts = types_df["Types"].value_counts().reset_index()
 type_counts.columns = ["Type", "Count"]
-
+print(pokemon_df.columns)
 # Criar gráfico de barras para a distribuição de tipos
-sns.barplot(x="Count", y="Type", data=type_counts, palette="viridis")
+sns.barplot(x="Count", y="Type", data=type_counts, hue="Type", palette="viridis", legend=False)
 plt.title("Distribution of Pokémon by Type")
 plt.xlabel("Count")
 plt.ylabel("Type")
 plt.tight_layout()
-plt.show()
+#plt.show(block=False) #Não mostra o gráfico
+plt.savefig("grafico.png")  # Salva o gráfico no disco
+plt.close()  # Fecha a janela do gráfico
 
-# Análise estatística: Média de ataque, defesa e HP por tipo
+# Análise estatistica: Média de ataque, defesa e HP por tipo
 mean_stats = types_df.groupby("Types")[["Attack", "Defense", "HP"]].mean().reset_index()
 mean_stats.columns = ["Type", "Mean Attack", "Mean Defense", "Mean HP"]
 print(mean_stats)
 
-# Top 5 Pokémon por experiência base
+
+# Top 5 Pokémon por experiencia base
 top_5_pokemon = pokemon_df.nlargest(5, "Base Experience")
 print("Top 5 Pokémon by Base Experience:")
 print(top_5_pokemon[["Name", "Base Experience"]])
+
+# Exportar tabela de média de ataque, defesa e HP por tipo
+mean_stats.to_csv("mean_stats.csv", index=False)
+print("Tabela de médias por tipo exportada para mean_stats.csv")
+
+# Exportar tabela dos 5 Pokémon com maior experiência base
+top_5_pokemon[["Name", "Base Experience"]].to_csv("top_5_pokemon.csv", index=False)
+print("Tabela dos Top 5 Pokémon exportada para top_5_pokemon.csv")
